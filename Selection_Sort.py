31c05339cf9386170c54f40d963ea696dc9c1ed2
An Chang Hyeon <61732687+Anchang8@users.noreply.github.com>
2020-05-15 11:18:10 +0900

    Update Selection_Sort.py

--- Selection_Sort.py
+++ Selection_Sort.py
@@ -1,7 +1,8 @@
 def Selection_Sort(l,k):
-    for i in range(0,k):
+    num=len(l)
+    for i in range(0,num-1):
         minindex=i
-        for j in range(i+1,k):
+        for j in range(i+1,num):
             if l[minindex]>l[j]:
                 minindex=j
     l[i],l[minindex]=l[minindex],l[i]
